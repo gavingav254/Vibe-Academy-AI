1310132f21d98c440083fedde539b59177ba3cce
gavin chesebe <gavingavy05@gmail.com>
2026-03-19 02:18:27 +0300

    Add dark theme color definitions

---
+++ lib/constants/colors.dart
@@ -0,0 +1,17 @@
+// lib/constants/colors.dart
+
+import 'package:flutter/material.dart';
+
+class DarkThemeColors {
+  static const Color primary = Color(0xFF1E1E2F);
+  static const Color secondary = Color(0xFF2B2B45);
+  static const Color accent = Color(0xFF3A3A55);
+  static const Color background = Color(0xFF121212);
+  static const Color surface = Color(0xFF1B1B1B);
+  static const Color error = Color(0xFFCF6679);
+  static const Color onPrimary = Colors.white;
+  static const Color onSecondary = Colors.white;
+  static const Color onBackground = Colors.white;
+  static const Color onSurface = Colors.white;
+  static const Color onError = Colors.black;
+}
