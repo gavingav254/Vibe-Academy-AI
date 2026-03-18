cbdb54dea038d29bede99319a4c571825b7d502f
gavin chesebe <gavingavy05@gmail.com>
2026-03-19 02:56:08 +0300

    Add FirebaseOptions for Android platform
    Added DefaultFirebaseOptions class with Android configuration.

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform => android;
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'YOUR_ANDROID_API_KEY',
+    appId: 'YOUR_ANDROID_APP_ID',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    databaseURL: 'https://YOUR_PROJECT_ID.firebaseio.com',
+    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+  );
+}
